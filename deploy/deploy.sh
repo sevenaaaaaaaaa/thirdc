cd237fd8af042e930e19a26db98b00c9fb0561fa
DaydreamHeart <sevenaaaaaaa@nownexts.com>
2026-09-21 09:22:41 +0800

    chore: deploy 排除本地镜像/密钥/插件状态目录，gitignore 补 .v2c/.video_agent
    deploy.sh 的 rsync 原排除清单没盖住 server-kb/（1.4G 本地镜像）与 .deploy-ssh/
    （部署私钥），每次部署会白传一遍；补上排除。.v2c/.video_agent 为本地插件
    状态，加 gitignore 防止误提交。

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,6 +9,8 @@ SSH_CMD="ssh -i ${SSH_KEY} -p ${SSH_PORT} -o StrictHostKeyChecking=no ${USER_}@$
 SKIP_BUILD=${1:-}
 rsync -az --delete -e "ssh -i ${SSH_KEY} -p ${SSH_PORT} -o StrictHostKeyChecking=no" \
   --exclude '.git' --exclude 'target' --exclude '.thirdc' \
+  --exclude 'server-kb' --exclude '.deploy-ssh' \
+  --exclude '.v2c' --exclude '.video_agent' --exclude '.DS_Store' \
   ./ "${USER_}@${HOST}:${REMOTE_DIR}/src/"
 $SSH_CMD "cd ${REMOTE_DIR}/src && ${SKIP_BUILD:+echo skip-build || }cargo build --release -p thirdc 2>&1 | tail -1; mkdir -p bin kb && cp target/release/thirdc bin/ && true"
 $SSH_CMD "cp ${REMOTE_DIR}/src/deploy/thirdc.service /etc/systemd/system/ && systemctl daemon-reload && systemctl enable --now thirdc"
